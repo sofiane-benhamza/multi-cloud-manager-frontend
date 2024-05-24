f8118c92b3370503bd0b063c92384a96367d7c70
BENHAMZA Sofiane <71550283+sofiane-benhamza@users.noreply.github.com>
2024-05-24 12:11:54 +0100

    Update index.js

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -141,7 +141,6 @@ export default function Signup({ setWarning }) {
                                                 id="password"
                                                 name="password"
                                                 placeholder="my-pa$$w0rd"
-                                                pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
                                                 title="Minimum eight characters, at least one uppercase letter, one lowercase letter and one number"
                                                 value={personnalInfo.password}
                                                 className="form-control"
